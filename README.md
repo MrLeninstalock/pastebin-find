45c6601c194f64a5314f1cd75900780a9abf0a3b
Baptiste <baptiste.cauchard@hotmail.fr>
2019-06-14 17:00:53 +0200

    Save findings in ./Found/word.txt file

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ pastebin-find
 
 Python script to monitor new Pastebin pastes for a provided search term
 
-Usage: python pastefind.py search_regex
+Usage: python pastefind.py 
 
 Edit the "time_between" variable to change the time between requests. Currently it is at 5s. If the value is too low, pastebin may block your IP address for repeated queries. If it is too high, you may miss a paste since the program only scans the top 10 pastes on the recently added page.
 
@@ -12,7 +12,7 @@ Regex should be the regex itself in quotes. For example, to search for formatted
 Objectif
 ========
 
-+ Be able to search for KeyWord instead of regex
-+ Save the text not only the link (the link may be deleted before we click it)
++ Avoid scanning same file numerous time
++ Save extract of findings
 + Make some cool stats (when are the most file uploaded etc)
 + Mail to owner when something is found
--- pastefind.py
+++ pastefind.py
@@ -11,13 +11,6 @@ import re
 time_between =  20      #Seconds between iterations (not including time used to fetch pages - setting below 5s may cause a pastebin IP block, too high may miss pastes)
 error_on_cl_args = "Please provide a single regex search via the command line"   #Error to display if improper command line arguments are provided
 
-# Check for command line argument (a single regex)
-if len(sys.argv) != 1:
-    search_term = sys.argv[1]
-else:
-    print error_on_cl_args
-    exit()
-
 iterater = 1
 
 wordlist_file = open("toFind.txt", "r")
@@ -30,12 +23,13 @@ while(1):
     print "Scanning pastebin - iteration " + str(iterater) + "..."
     
     # Open the recently posted pastes page
-    url = urllib.urlopen("http://pastebin.com/archive")
-    html = url.read()
-    url.close()
+    #url = urllib.urlopen("http://pastebin.com/archive")
+    #html = url.read()
+    #url.close()
 
     # Capture all pastebin id's
-    id_list = re.findall('href="\/([a-zA-Z1-9]{8})"', html)
+    #id_list = re.findall('href="\/([a-zA-Z1-9]{8})"', html)
+    id_list = ["1","2","3","4","5","6"]
     if "messages" in id_list:
         id_list.remove("messages")
     if "settings" in id_list:
@@ -43,15 +37,18 @@ while(1):
 
     for id in id_list:
         #Begin loading of raw paste text
-        url_2 = urllib.urlopen("https://pastebin.com/raw/" + id)
-        raw_text = url_2.read()
-        url_2.close()
+        #url_2 = urllib.urlopen("https://pastebin.com/raw/" + id)
+        #raw_text = url_2.read()
+        #url_2.close()
+        raw_text = open("tmmp", "r").read()
         
         # TODO : Use a file of keyword to find
         for word in wordlist:
             if re.search(word, raw_text):
                 print "FOUND " + word + " in http://pastebin.com/raw.php?i=" + id
-        
+                f = open("./Found/"+word+".txt", "a")
+                f.write(id + "\n")
+                f.close()
         counter += 1
     iterater += 1
     time.sleep(time_between)
