633b9c2739eb625321c895d8aec91162d1a3575f
Baptiste <baptiste.cauchard@hotmail.fr>
2019-06-14 09:42:11 +0200

    Better way to get all paste id.

--- pastefind.py
+++ pastefind.py
@@ -8,7 +8,7 @@ import re
 
 # User-defined variables
 # TODO : Use a config file : https://docs.python.org/2/library/configparser.html
-time_between = 7       #Seconds between iterations (not including time used to fetch pages - setting below 5s may cause a pastebin IP block, too high may miss pastes)
+time_between =  20      #Seconds between iterations (not including time used to fetch pages - setting below 5s may cause a pastebin IP block, too high may miss pastes)
 error_on_cl_args = "Please provide a single regex search via the command line"   #Error to display if improper command line arguments are provided
 
 # Check for command line argument (a single regex)
@@ -25,33 +25,26 @@ while(1):
 
     print "Scanning pastebin - iteration " + str(iterater) + "..."
     
-    #Open the recently posted pastes page
-    try:
-        url = urllib.urlopen("http://pastebin.com/archive")
-        html = url.read()
-        url.close()
-        html_lines = html.split('\n')
-        for line in html_lines:
-            if counter < 10:
-                # TODO : Use a file of keyword to find
-                if re.search(r'<td><img src=\"/i/t.gif\"  class=\"i_p0\" alt=\"\" border=\"0\" /><a href=\"/[0-9a-zA-Z]{8}">.*</a></td>', line):
-                    link_id = line[72:80]
-                    #print link_id
-                    
-                    #Begin loading of raw paste text
-                    url_2 = urllib.urlopen("http://pastebin.com/raw.php?i=" + link_id)
-                    raw_text = url_2.read()
-                    url_2.close()
-                    
-                    #if search_term in raw_text:
-                    if re.search(r''+search_term, raw_text):
-                        print "FOUND " + search_term + " in http://pastebin.com/raw.php?i=" + link_id
-                    
-                    counter += 1
-    except(IOError):
-        print "Network error - are you connected?"
-    except:
-        print "Fatal error! Exiting."
-        exit()
+    # Open the recently posted pastes page
+    url = urllib.urlopen("http://pastebin.com/archive")
+    html = url.read()
+    url.close()
+    
+    # Capture all pastebin id's
+    id_list = re.findall('href="\/([a-zA-Z1-9]{8})"', html)
+    id_list.remove("messages")
+    id_list.remove("settings")
+
+    for id in id_list:
+        #Begin loading of raw paste text
+        url_2 = urllib.urlopen("https://pastebin.com/raw/" + id)
+        raw_text = url_2.read()
+        url_2.close()
+        
+        # TODO : Use a file of keyword to find
+        if re.search(r''+search_term, raw_text):
+            print "FOUND " + search_term + " in http://pastebin.com/raw.php?i=" + id
+        
+        counter += 1
     iterater += 1
     time.sleep(time_between)
